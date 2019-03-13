9ec4cac19314b2b89c8aeeffffa8070119939a95
Wonil Park <wpark912@gmail.com>
2019-03-13 09:36:39 -0400

    Planet Class added

---
+++ AwesomeSpaceGame/Planet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeSpaceGame
+{
+    class Planet
+    {
+        double x, y;
+        string name;
+
+        public Planet()
+        {
+
+        }
+    }
+}
