b95d4d4ad02a82c760f00a8e1df0593b63ba03a4
Wonil Park <wpark912@gmail.com>
2019-03-14 11:28:53 -0400

    Space Forces

--- AwesomeSpaceGame/Display.cs
+++ AwesomeSpaceGame/Display.cs
@@ -3,15 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace AwesomeSpaceGame
 {
     class Display
     {
-        public void Title()
-        { 
-          Console.ForegroundColor = ConsoleColor.Green;
-          string title = @"
+        int selectedItem = 0;
+            List<string> mainMenu = new List<string>();
+
+        public void Run()
+        {
+            MainPage();
+        }
+
+
+        private void MainPage()
+        {
+            ASCIIMain();
+            Console.WriteLine("\n\n\n                                         Press any key to sign your CONTRACT...");
+
+            string script = "\n\n\n" +
+                "\n                                                   Earth needs YOU... " +
+                "\n                                                    Future WARRIOR" +
+                "\n                                               LOYALTY, DUTY, RESPECT," +
+                "\n                                         SELFLESS SERVICE, HONOR, INTEGRITY" +
+                "\n                                             and.....PERSONAL.....COURAGE!! " +
+                "\n                                     Visit the nearest ARMY recruiting center!! ";
+            foreach (char c in script)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write(c);
+                Thread.Sleep(40);
+            }
+            Console.ReadKey();
+
+            bool valid = false;
+            Console.Write("What is your name, warrior?: ");
+            string userName = Console.ReadLine();
+
+        
+            do
+            {
+                Console.Clear();
+                ASCIIMain();
+                mainMenu.Add("Start");
+                mainMenu.Add("");
+                mainMenu.Add("Quit");
+                for (int i = 0; i < mainMenu.Count; ++i)
+                {
+                    if (selectedItem == i)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                    }
+                    Console.WriteLine(mainMenu[selectedItem]);
+                }                
+
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        SelectNextItem();
+                        break;
+                    case ConsoleKey.Enter:
+                        break;
+                }
+
+            } while (!valid);
+
+
+        }
+
+        private static void ASCIIMain()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string title = @"
                                _____                          __  __                __         
                               / ___/____  ____ _________     / / / /_  ____________/ /__  _____
                               \__ \/ __ \/ __ `/ ___/ _ \   / /_/ / / / / ___/ ___/ / _ \/ ___/
@@ -21,11 +87,17 @@ namespace AwesomeSpaceGame
 
             Console.WriteLine(title);
             Console.ResetColor();
-            Console.WriteLine("\n\n\n                                         Press any key to start DA HUSTLE...");
-            Console.ReadKey();
-
-            
         }
+
+        private void SelectNextItem()
+        {
+            selectedItem += 1;
+            if (selectedItem >= mainMenu.Count)
+            {
+                selectedItem = 0;
+            }           
+        }
+
 
 
 
--- AwesomeSpaceGame/Program.cs
+++ AwesomeSpaceGame/Program.cs
@@ -10,7 +10,7 @@ namespace AwesomeSpaceGame
     {
         static void Main(string[] args)
         {
-            new Display().Title();           
+            new Display().Run();           
             
             
         }
