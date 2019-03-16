888e1eb0ad568e8156b36210bb2125c38800e78c
Alex Gehn <alexgehn1994@gmail.com>
2019-03-16 03:03:37 -0700

    Got name entered,need to move to character class and pull from there

--- AwesomeSpaceGame/Character.cs
+++ AwesomeSpaceGame/Character.cs
@@ -6,38 +6,39 @@ using System.Threading.Tasks;
 
 namespace AwesomeSpaceGame
 {
-    //class Character
-    //{
-    //    //characteristics    
-    //    public string name;
-    //    const int startAge = 20;
-    //    public double money;
-    //    public double age;
-    //    int select;
-
-
-    //    //constructor
-    //    public void CreateCharacter(string name)
-    //    {
-    //        this.name = name;
-    //        this.money = 1000;
-    //        this.age = startAge;
-    //    }
-    //    //Add money to player
-    //    public void AddMoney(int deposit)
-    //    {
-    //        money += deposit;
-    //    }
-    //    //Take money from player
-    //    public void TakeMoney(int subtract)
-    //    {
-    //        money -= subtract;
-    //    }
-
-    //    //public void displayCharacter()
-    //    //{
-    //    //    Console.WriteLine($"Your characters name is{name}, you have {money}, you are {age} old")
-    //    //}
-
-    //}
+    class Character
+    {
+        //characteristics    
+        public string name;
+        const int startAge = 20;
+        public double money;
+        public double age;
+        int select;
+    }
 }
+
+
+        //    //constructor
+        //    public void CreateCharacter(string name)
+        //    {
+        //        this.name = name;
+        //        this.money = 1000;
+        //        this.age = startAge;
+        //    }
+        //    //Add money to player
+        //    public void AddMoney(int deposit)
+        //    {
+        //        money += deposit;
+        //    }
+        //    //Take money from player
+        //    public void TakeMoney(int subtract)
+        //    {
+        //        money -= subtract;
+        //    }
+
+        //    //public void displayCharacter()
+        //    //{
+        //    //    Console.WriteLine($"Your characters name is{name}, you have {money}, you are {age} old")
+        //    //}
+
+        //}
--- AwesomeSpaceGame/Program.cs
+++ AwesomeSpaceGame/Program.cs
@@ -8,28 +8,50 @@ namespace AwesomeSpaceGame
 {
     public class Program
     {
-        
 
+        
         static void Main(string[] args)
         {       
-        new Display().Run();
         
+        new Display().Run();
+            Console.Clear();
+        Character One = new Character();
+            Console.WriteLine("What is your Name.");
+            string userInput = Console.ReadLine();
+            string name = userInput;
+            One.name = name;
+            Console.Clear();
+            Console.WriteLine($"{One.name} you are now part of the worlds greatest military in the universe,\n you will get lots of women and lots of money.......");
+            Console.WriteLine("\n\n\n\n ........3 years later\n It is time for you to get out of the military.But if you get out now you will not be able to pay for “blank”.\n You are on leave for 50 days and by the end you have to earn $200,000 or reenlist\n ...FOR LIFE");
+            Console.ReadLine();
+      
+       
+
+            //Console.WriteLine("Enter warp factor: ");
+            //double warpFactor = Convert.ToDouble(Console.ReadLine());
+
+            //new SpaceShip().Speed(warpFactor);
+            //var earth = new Planet("Earth", 0.0, 0.0);
+            //var alpha = new Planet("Alpha Centauri 3", 2.3, 3.7);
+            //var eridani = new Planet("40 Eridani", 16.6, -35.2);
+
+            //double distance = new Planet().Distance(0.0, 0.0, 2.3, 3.7);
+            //Console.WriteLine($"{}'s spped is {distance}");
 
 
-        //Console.WriteLine("Enter warp factor: ");
-        //double warpFactor = Convert.ToDouble(Console.ReadLine());
-
-        //new SpaceShip().Speed(warpFactor);
-        //var earth = new Planet("Earth", 0.0, 0.0);
-        //var alpha = new Planet("Alpha Centauri 3", 2.3, 3.7);
-        //var eridani = new Planet("40 Eridani", 16.6, -35.2);
-
-        //double distance = new Planet().Distance(0.0, 0.0, 2.3, 3.7);
-        //Console.WriteLine($"{}'s spped is {distance}");
 
 
+        }
 
+        public string createNameTwo(string name)
+        {
+            Character One = new Character();
+            One.name = name;
+            return name;
+        }
 
-    }
+        public void createName()
+        {
+        }
     }
 }
