75f6ab48bd2bc726719d3fde43ea8faaebe4b78e
Wonil Park <wpark912@gmail.com>
2019-03-16 21:15:42 -0400

    intro moved under character class

--- AwesomeSpaceGame/Character.cs
+++ AwesomeSpaceGame/Character.cs
@@ -15,6 +15,14 @@ namespace AwesomeSpaceGame
         double money;
         double age;
         int select;
+
+        public string Intro(string userName)
+        {
+            string intro = $"{userName} you are now part of the worlds greatest military in the universe,\n you will get lots of women and lots of money.......\n\n\n\n ........3 years later\n It is time for you to get out of the military.But if you get out now you will not be able to pay for “blank”.\n You are on leave for 50 days and by the end you have to earn $200,000 or reenlist\n ...FOR LIFE";
+            Console.Clear();
+            Console.WriteLine();
+            return intro;
+        }
     }
 }
 
